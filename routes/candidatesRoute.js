266cb6521e5767198c556bd96986b7b3e8da3898
apnacolege git demo class <kash@gmail.com>
2025-01-13 23:04:26 +0530

    updated code

--- routes/candidatesRoute.js
+++ routes/candidatesRoute.js
@@ -1,3 +1,4 @@
+const mongoose = require("mongoose");
 const express = require('express');
 const candidatesModel = require("../models/candidates.js");
 const bcrypt = require("bcrypt");
@@ -9,8 +10,9 @@ const { isAdmin,isLoggedin } = require('./verifyToken.js');
 route.post("/", isAdmin, async (req,res)=>{
     try{
         const data =req.body
-    const newcandidate=new candidates(data);
-    const response= await newcandidate.save();
+        
+    const newcandi=new candidates(data);
+    const response= await newcandi.save();
 
     res.status(201).json({message:"candidate created successfully",response});
     
@@ -58,19 +60,23 @@ route.post("/", isAdmin, async (req,res)=>{
 
  /// Lets start Voting 
 
- route.post("/vote/:candidateID", isLoggedin, async(req,res)=>{
+ route.post("/vote/:id", isLoggedin, async(req,res)=>{
     // admin cant vote 
     // user can only vote once 
     // we need two things to vote ... 1 user id who the person vote ..will extract the id from isLoogedin token... 2 candidatesID to whome user will vote ....candidateid getting from parameter
-//    const candId=req.params.candidateID;
-    // const userID=req.user.id;
+ 
 
     try{
-        const {candidateID}=req.params;
+        const extractCandidateID =req.params.id;
         const userID = req.user.id;
+            // Validate the candidate ID
+
+        if (!mongoose.Types.ObjectId.isValid(extractCandidateID)) {
+            return res.status(400).json({ message: "Invalid Candidate ID" });
+          }
 
         // find the candidates document with specific candidateID
-        const candidate = await candidatesModel.findById(candidateID);
+        const candidate = await candidatesModel.findById(extractCandidateID);
         if (!candidate) {
             return res.status(404).json({ message: "Candidate not found" });
         }
@@ -83,40 +89,73 @@ route.post("/", isAdmin, async (req,res)=>{
             return res.status(400).json({ message: "User has already voted" });
         }
         
-        if (user.role === "admin") {
+        if (user.role === 'admin') {
             return res.status(403).json({ message: "Admins are not allowed to vote" });
         }
         
 
-        /// next step ..if user voted then we have to make isvoted deafult false to true in userSchema ....and 2 task is to we have to save the vote and userid in candidated Schema 
+        /// next step ..if user voted then we have to make isvoted deafult false to true in userSchema ....
+        // and 2 task is to we have to save the vote and userid in candidated Schema 
 
 
         /// updating Candidate Schema to record the vote 
-        candidate.votes.push({ user: userID }); // Record user's vote
-         candidate.votecount += 1; // Increment vote count
+        candidate.votes.push({ user: userID }); // passing userId in user .. Record user's vote
+         candidate.votecount  += 1; // Increment vote count
          console.log("Candidate after updating votes:", candidate); // Debugging log
-
-         user.isVoted = true; // Mark user as having voted
-
-         console.log("User after updating isVoted:", user); // Debugging log
-
          await candidate.save();
 
-        await user.save();
 
-        
+
+         /// update the user document  
+
+         user.isVoted = true; // Mark user as having voted
+         console.log("User after updating isVoted:", user); // Debugging log
+         await user.save();
+
+         
 
         res.status(200).json({message:"Vote recorded Successfully"});
 
 
 
     }catch(err){
-        res.status(500).json({error:"internal server error"});
+        res.status(500).json({error:"internal server error",details:err.message});
 
     }
 
  })
 
+
+  //// vote count in sorted order ..how many vote each party gain //
+
+  route.get("/vote/count",async(req,res)=>{
+    try{
+        /// find all candidates and sort them by votecount in descending order 
+        const candidate =await candidatesModel.find().sort({votecount:'desc'})
+
+        /// map the candidates to only return thier name and votecount
+
+        const voterecord = candidate.map((data)=>{
+            return{
+                name:data.name,
+                party:data.party,
+                count:data.votecount
+
+            }
+
+        })
+        
+        return res.status(200).json(voterecord)
+
+
+    }catch(err){
+        res.status(500).json({error:"internal server error"});
+
+    }
+     
+
+ })
+  
 
 
 
